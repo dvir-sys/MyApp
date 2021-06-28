e4066ea35bc383ba342c6dee7576c027587e853a
dvir-sys <dvirleonardo28@gmail.com>
2021-06-28 06:14:50 +0300

    todolist

--- API/Controllers/TodoListController.cs
+++ API/Controllers/TodoListController.cs
@@ -1,4 +1,8 @@
+using API.Data;
+using API.Entities;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,12 +13,26 @@ namespace API.Controllers
 {
     public class TodoListController : BaseApiController
     {
-        public TodoListController(){
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
 
+        public TodoListController(DataContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoList>>> GetAllLists(){
+            return await _context.TodoLists.ToListAsync();
+        }
 
-        
+        [Route("/lists/:id")]
+        [HttpGet]
+         public async Task<ActionResult<TodoList>> GetListById(int id){
+            return await _context.TodoLists.FindAsync(id);
+         }
+        /*
         [HttpGet]
         // api/todoList
         
@@ -23,7 +41,7 @@ namespace API.Controllers
             return "C";
         }
       
-    
+    */
 
 
 
